616224d64294e16ebd55e015cf30f0b5fe46b740
ViktorSev86 <butioxa_86@mail.ru>
2023-02-23 21:00:42 +0300

    Shablon

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -19,15 +19,22 @@ class AppAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
-    public $css = [
+    public $css = [ // Подключаем файлы стилей, упомянутые в шаблоне grocery
         'css/bootstrap.css',
         'css/style.css',
         'css/font-awesome.css',
         '//fonts.googleapis.com/css?family=Ubuntu:400,300,300italic,400italic,500,500italic,700,700italic',
         '//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic',
+        'css/flexslider.css',
     ];
-    public $js = [
+    public $js = [ // Полключаем файлы js, упомянутые в шаблоне grocery
         'js/jquery-1.11.1.min.js',
+        'js/bootstrap.min.js',
+        'js/move-top.js',
+        'js/easing.js',
+        'js/jquery.flexslider.js',
+        'js/minicart.js',
+        'js/main.js', // Подключаем файл, который мы создали
     ];
     public $depends = [
         'yii\web\YiiAsset',
